ba47023efe93a3d6c57bc89ebe06db0070a651b7
Jozef Mikušinec <mik.jozef@gmail.com>
2022-09-24 09:27:12 +0200

    One last attempt at full generality
    Ok so I realized 2 things.
    
    0. There actually are people I can bother with my Lean-related
    quesitons. Mamy thanks to the people of
    proofassistants.stackexchange.com who helped me when I was stuck.
    
    1. I gave my ideal of 1-1 correspondence another try. Becoming
    more capable as a Lean programmer, I think it might not be that
    prohibitive after all. On the other hand, it probably isn't
    useful either.

--- lean/bst.lean
+++ lean/bst.lean
@@ -1,49 +1,195 @@
+/-
+  Code style: prefer no tactics.
+-/
+
 open Classical
+
+
+
+-- # Chapter 0: Sets and other basic definitions
+-- =============================================
+
+-- The square less-equal relation: `x ⊑ y`.
+class SqLE (α : Type u) where
+  le : α → α → Prop
+
+-- The square less-equal relation: `x ⊏ y`.
+class SqLT (α : Type u) where
+  lt : α → α → Prop
+
+infix:50 " ⊑ " => SqLE.le
+infix:50 " ⊏ " => SqLT.lt
+
+
+class PartialOrder (T: Type) extends LE T, LT T where
+  refl (t: T): t ≤ t
+  antisymm (a b: T): a ≤ b  →  b ≤ a  →  a = b
+  trans (a b c: T): a ≤ b  →  b ≤ c  →  a ≤ c
+  
+  lt (a b: T) := a ≤ b  ∧  ¬ a = b
+  ltIffLeNotEq (a b: T): a < b  ↔  a ≤ b ∧ ¬ a = b
+
+-- TODO Can I combine PartialOrder with PartialOrderSq into one definition?
+class PartialOrderSq (T: Type) extends SqLE T, SqLT T where
+  refl (t: T): t ⊑ t
+  antisymm (a b: T): a ⊑ b  →  b ⊑ a  →  a = b
+  trans (a b c: T): a ⊑ b  →  b ⊑ c  →  a ⊑ c
+  
+  lt (a b: T) := a ⊑ b  ∧  ¬ a = b
+  ltIffLeNotEq (a b: T): a ⊏ b  ↔  a ⊑ b ∧ ¬ a = b
+
 
 def Set (T : Type) := T → Prop
 
 instance: Membership T (Set T) where
   mem := fun (t: T) (s: Set T) => s t
 
--- Does not work... Cannot use sets as types with this.
---instance: Coe (Set T) Type where
---  coe s := { t: T // t ∈ s }
+instance: Coe (Set T) Type where
+  coe s := { t: T // t ∈ s }
 
 
--- TODO delete? Semantically, there is a difference, but
--- character-count-wise, it's not simpler than the function
--- type. TODO do I even use it?
--- 
--- def Family {D: Type} (T: Type) := D → T
+theorem Set.ext {a b : Set D} (h : ∀ x, x ∈ a ↔ x ∈ b) : a = b :=
+  funext (fun x => propext (h x))
+
+
+instance: LE (Set D) where
+  le (a: Set D) (b: Set D): Prop := ∀ d: D, d ∈ a → d ∈ b
+
+instance: PartialOrder (Set D) where
+  le (a: Set D) (b: Set D): Prop := ∀ d: D, d ∈ a → d ∈ b
+  
+  refl (_: Set D) := fun _: D => id
+  
+  antisymm (a b: Set D) :=
+    fun (ab: a ≤ b) (ba: ∀ d: D, d ∈ b → d ∈ a) =>
+      let abElem: ∀ d: D, d ∈ a ↔ d ∈ b := fun (s: D) => Iff.intro (ab s) (ba s);
+      Set.ext abElem
+  
+  trans (a b c: Set D) := fun (ab: a ≤ b) (bc: b ≤ c) =>
+    -- In general, do I prefer long and incremental and explicit proofs,
+    -- or terse and unreadable monsters? I think I prefer the former.
+    --
+    -- fun (d: D) =>
+    --   let abi: d ∈ a → d ∈ b := ab s
+    --   let bci: d ∈ b → d ∈ c := bc s
+    --   fun (sa: d ∈ a) => bci (abi sa)
+    fun (d: D) (sa: d ∈ a) => (bc d) ((ab d) sa)
+  
+  ltIffLeNotEq _ _ := Iff.intro id id
 
 namespace Set
-  -- Should the implication be turned into an equivalence?
-  def isFinite (s: Set T): Prop := ∃ l: List T, ∀ t: T, t ∈ s → t ∈ l
+  def isFinite (s: Set D): Prop := ∃ l: List D, ∀ t: D, t ∈ s → t ∈ l
   
-  def union {D: Type} {T: Type}: (D → Set T) → Set T :=
-    fun (family: D → Set T) (t: T) => ∃ d: D, family d t
+  def isSubset (a b: Set D): Prop := ∀ d: D, d ∈ a → d ∈ b
+  
+  def union {Index: Type} {D: Type} (family: Index → Set D): Set D :=
+    fun (d: D) => ∃ i: Index, family i d
+  
+  theorem unionIsWider
+    (family: Index → Set D)
+    (i: Index)
+  :
+    isSubset (family i) (union family)
+  :=
+    sorry
+  
+  def binaryUnion (a b: Set D): Set D := fun d: D => a d ∨ b d
+  def binaryIntersection (a b: Set D): Set D := fun d: D => a d ∧ b d
 end Set
 
-def FinSet (T: Type) := { s: Set T // Set.isFinite s }
+infix:50 " ⊆ " => Set.isSubset
+infix:60 " ∪ " => Set.binaryUnion
+infix:60 " ∩ " => Set.binaryIntersection
 
 
 
-def Signature := (Op: Type) × (Op → Nat)
+-- # Chapter 1: Well-founded collections
+-- =====================================
 
-def sigOp (Op: Type) := { s: Signature // s.fst = Op }
-def arity {Op: Type} (op: Op) (s: sigOp Op): Nat :=
-  s.val.snd op
+structure Set3 (D: Type) where
+  defMem: Set D
+  posMem: Set D
+  defLePos: defMem ≤ posMem
 
+protected def Set3.eq:
+  (a b: Set3 D) →
+  a.defMem = b.defMem →
+  a.posMem = b.posMem
+→
+  a = b
+-- Thanks to answerers of https://proofassistants.stackexchange.com/q/1747
+| ⟨_, _, _⟩, ⟨_, _, _⟩, rfl, rfl => rfl
+
+
+instance: LE (Set3 D) where
+  le (a b: Set3 D) := a.defMem ≤ b.defMem  ∧  a.posMem ≤ b.posMem
+
+instance: SqLE (Set3 D) where
+  le (a b: Set3 D) := a.defMem ≤ b.defMem  ∧  b.posMem ≤ a.posMem
+
+
+instance: PartialOrder (Set3 D) where
+  refl (a: Set3 D) :=
+    And.intro (PartialOrder.refl (a.defMem)) (PartialOrder.refl (a.posMem))
+  
+  antisymm (a b: Set3 D) (ab: a ≤ b) (ba: b ≤ a) :=
+    let defEq: a.defMem = b.defMem :=
+      PartialOrder.antisymm a.defMem b.defMem ab.left ba.left;
+    let posEq: a.posMem = b.posMem :=
+      PartialOrder.antisymm a.posMem b.posMem ab.right ba.right;
+    Set3.eq a b defEq posEq
+  
+  trans (a b c: Set3 D) (ab: a ≤ b) (bc: b ≤ c) :=
+    And.intro
+      (PartialOrder.trans a.defMem b.defMem c.defMem ab.left bc.left)
+      (PartialOrder.trans a.posMem b.posMem c.posMem ab.right bc.right)
+  
+  ltIffLeNotEq _ _ := Iff.intro id id
+
+
+instance: PartialOrderSq (Set3 D) where
+  refl (a: Set3 D) :=
+    And.intro (PartialOrder.refl (a.defMem)) (PartialOrder.refl (a.posMem))
+  
+  antisymm (a b: Set3 D) (ab: a ⊑ b) (ba: b ⊑ a) :=
+    let defEq: a.defMem = b.defMem :=
+      PartialOrder.antisymm a.defMem b.defMem ab.left ba.left;
+    let posEq: a.posMem = b.posMem :=
+      PartialOrder.antisymm a.posMem b.posMem ba.right ab.right;
+    Set3.eq a b defEq posEq
+  
+  trans (a b c: Set3 D) (ab: a ⊑ b) (bc: b ⊑ c) :=
+    And.intro
+      (PartialOrder.trans a.defMem b.defMem c.defMem ab.left bc.left)
+      (PartialOrder.trans c.posMem b.posMem a.posMem bc.right ab.right)
+  
+  ltIffLeNotEq _ _ := Iff.intro id id
+
+
+-- Thanks to answerers of https://proofassistants.stackexchange.com/q/1740
+def Signature := (Op: Type) × (Op → Type)
+def Op (s: Signature) := s.fst
+
+def SigOp (Op: Type) := { s: Signature // s.fst = Op }
+def arity {Op: Type} (op: Op) (s: SigOp Op): Type := s.val.snd (cast s.property.symm op)
+
+
+inductive ArityZero
+inductive ArityOne
+| zth
+inductive ArityTwo
+| zth
+| fst
 
 inductive NatOp
   | zero
   | succ
   | plus
 
-def natAr: NatOp → Nat
-  | NatOp.zero => 0
-  | NatOp.succ => 1
-  | NatOp.plus => 2
+def natAr: NatOp → Type
+  | NatOp.zero => ArityZero
+  | NatOp.succ => ArityOne
+  | NatOp.plus => ArityTwo
 
 def natSig: Signature := ⟨NatOp, natAr⟩
 
@@ -52,65 +198,137 @@ inductive PairOp where
   | zero
   | pair
 
-def pairAr: PairOp → Nat
-  | PairOp.zero => 0
-  | PairOp.pair => 2
+def pairAr: PairOp → Type
+  | PairOp.zero => ArityZero
+  | PairOp.pair => ArityTwo
 
 def pairSig: Signature := ⟨PairOp, pairAr⟩
 
 
 
 def Variable := Nat
-def VarSet := Set Variable
-def FinVarSet := FinSet Variable
+-- Why tf is "reducible" even required? Lean, this is so fucking retarded.
+@[reducible] def VarSet := Set Variable
 
-def varAdd (Var: VarSet) (x: Variable): VarSet :=
+def addVar (Var: VarSet) (x: Variable): VarSet :=
   fun z => Var z ∨ z = x
 
-mutual
-  inductive VectExpr (s: Signature): (Var: VarSet) → Nat → Type
-    | nil: VectExpr s Var 0
-    | cons: Expr s Var → {n: Nat} → VectExpr s Var (n+1)
-  
-  inductive Expr (s: Signature): (Var: VarSet) → Type
-    | var: { x: Variable // Var x } → Expr s Var
-    | opApp:
-        (op: s.fst) →
-        VectExpr s Var (arity op ⟨s, rfl⟩) →
-        Expr s Var
-    | un: Expr s Var → Expr s Var → Expr s Var
-    | ir: Expr s Var → Expr s Var → Expr s Var
-    | cpl: Expr s Var → Expr s Var
-    | Un: (x: Variable) → Expr s (varAdd Var x) → Expr s Var
-    | Ir: (x: Variable) → Expr s (varAdd Var x) → Expr s Var
-end
+def addVarPreservesIsWider
+  (Var: VarSet)
+  (WiderVar: VarSet)
+  (isWider: Var ⊆ WiderVar)
+  (x: Variable)
+:
+  addVar Var x ⊆ addVar WiderVar x
+:=
+  fun (v: Variable) (avx: v ∈ addVar Var x) =>
+    Or.elim avx
+      (fun vVar: Var v => Or.inl (isWider v vVar))
+      (fun vx: v = x => Or.inr vx)
 
-def FamExpr (s: Signature) (Var: VarSet) := { x: Variable // Var x } → Expr s Var
+
+inductive Expr (s: Signature): (Var: VarSet) → Type
+  | var: { x: Variable // Var x } → Expr s Var
+  | opApp:
+      (op: s.fst) →
+      (arity op ⟨s, _⟩ → Expr s Var) →
+      Expr s Var
+  | un: Expr s Var → Expr s Var → Expr s Var
+  | ir: Expr s Var → Expr s Var → Expr s Var
+  | cpl: Expr s Var → Expr s Var
+  | Un: (x: Variable) → Expr s (addVar Var x) → Expr s Var
+  | Ir: (x: Variable) → Expr s (addVar Var x) → Expr s Var
+
+
+-- This is horrendous. Please tell me I just suck at Lean
+-- and there is a much better way of doing this.
+def widenExpr
+  (expr: Expr s Var)
+  (WiderVar: VarSet)
+  (isWider: Var ⊆ WiderVar)
+:
+  Expr s WiderVar
+:=
+  match expr with
+  | Expr.var ⟨x, xInVar⟩ => Expr.var ⟨x, isWider x xInVar⟩
+  | Expr.opApp op exprs => Expr.opApp op (fun ar => widenExpr (exprs ar) WiderVar isWider)
+  | Expr.un a b => Expr.un (widenExpr a WiderVar isWider) (widenExpr b WiderVar isWider)
+  | Expr.ir a b => Expr.ir (widenExpr a WiderVar isWider) (widenExpr b WiderVar isWider)
+  | Expr.cpl a => Expr.cpl (widenExpr a WiderVar isWider)
+  | Expr.Un x body =>
+      Expr.Un
+        x
+        (widenExpr
+          body
+          (addVar WiderVar x)
+          (addVarPreservesIsWider Var WiderVar isWider x)
+        )
+  | Expr.Ir x body =>
+      Expr.Ir
+        x
+        (widenExpr
+          body
+          (addVar WiderVar x)
+          (addVarPreservesIsWider Var WiderVar isWider x)
+        )
+
+
+-- Coe.coe, why u no work?
+-- Family of sxpressions.
+def FamExpr (s: Signature) (Var: VarSet) := { v: Variable // Var v } → Expr s Var
 
 namespace FamExpr
   def isFinite (_: FamExpr s Var): Prop := Set.isFinite Var
   
-  noncomputable def union
-    {s: Signature}
-    --{D: Type}
-    {V: /-D-/ Nat → VarSet}
-    {fin: ∀ n: Nat, Set.isFinite (V n)}
-    -- I suppose an arbitrary domain cannot work constructively, because
-    -- given a variable x, `union` does not known under which index `d: D`
-    -- to look to find the corresponding expression. Am I right?
-    -- Also, how would I implement this function classically?
-    --(family: (d: D) → FamExpr s (V d))
-    (family: (n: Nat) → FamExpr s (V n))
-  : FamExpr s (Set.union V)
+  structure FamFamExpr (s: Signature) (Index: Type) (V: Index → VarSet) where
+    family: (i: Index) → FamExpr s (V i)
+    -- TODO fix
+    exprsCompatible
+      (i j: Index)
+      (v: Variable)
+      (vVi: v ∈ V i)
+      (vVj: v ∈ V j)
+    :
+      widenExpr (family i ⟨v, vVi⟩) (Set.union V) (Set.unionIsWider V i) =
+      widenExpr (family j ⟨v, vVj⟩) (Set.union V) (Set.unionIsWider V j)
+  
+  noncomputable def union (family: FamFamExpr s Index V): FamExpr s (Set.union V) :=
+    fun vProp: ↑(Set.union V) =>
+      match vProp with
+      | ⟨v, vVar⟩ =>
+        let _: ∃i: Index, v ∈ V i := vVar;
+        let nonempty: Nonempty { i: Index // v ∈ V i} :=
+          match vVar with
+          | ⟨i, h⟩ => ⟨i, h⟩;
+        let iProp: { i: Index // v ∈ V i} := choice nonempty;
+        match iProp with
+        | ⟨i, prop⟩ =>
+          widenExpr (family.family i ⟨v, prop⟩) (Set.union V) (Set.unionIsWider V i)
+  
+  theorem unionIsWider
+    (family: FamFamExpr s Index V)
+    (i: Index)
+    (v: ↑(Set.union V))
+    (vVi: v.val ∈ V i)
+  :
+    union family v = widenExpr (family.family i ⟨v, vVi⟩) (Set.union V) (Set.unionIsWider V i)
   :=
-    fun x: { x: Variable // x ∈ Set.union V } =>
-      find x 0
-    where
-      find (x: Nat) (n: Nat): /-Expr s (Set.union V)-/ Nat :=
-        if V n x then 0 else 0
+    sorry
+  
+  def Set.union {Index: Type} {D: Type} (family: Index → Set D): Set D :=
+    fun (d: D) => ∃ i: Index, family i d
 end FamExpr
 
-def DefList (s: Signature) (Var: VarSet) := { fe: FamExpr s //  }
+structure DefList (s: Signature) (Var: VarSet)
+    -- {fin: ∀ i: Index, Set.isFinite (V i)}
+
+
+
+
+def StructInterpretation (D: Type) (s: Signature) :=
+  (op: Op s) → Vector D (arity op ⟨s, rfl⟩)
+
+def Structure := (D: Type) × (s: Signature) × StructInterpretation D s
 
 
 
@@ -140,23 +358,6 @@ def DefList (s: Signature) (Var: VarSet) := { fe: FamExpr s //  }
 
 
 
-
-
-
-
--- How to conditionally call `foo x` if `foo` is defined on `x: Nat`?
-def foo (_: { n: Nat // n = 3 }) := 42
-
---noncomputable def t (x: Nat) (): Nat := if x ∈ foo then foo x else 0
-
-noncomputable def t (x: Nat) (s: Set Nat): Nat := if x ∈ s then 1 else 0
-
-
--- How to implement this?
---uncomputable def finSetNatToList (s: Set Nat) (fin: isFinite s): { l: List Nat // ∀ n: Nat, n ∈ l ↔ n ∈ s } :=
-  --let ex : ∃ l: List Nat, ∀ n: Nat, n ∈ s → n ∈ l := fin;
-  --let ⟨l, p⟩ := ex; -- Lol no. Cannot extract data from Prop.
-  --0 asdf
   
 
 
