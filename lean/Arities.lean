178d5bfee73493dd384907f274f87206d3a30b1b
Jozef Mikušinec <mik.jozef@gmail.com>
2024-04-01 21:43:50 +0200

    arities ofnat

--- lean/Arities.lean
+++ lean/Arities.lean
@@ -8,3 +8,8 @@ def ArityZero.noInst: ArityZero → False := ArityZero.rec
 -- "code generator does not support recursor 'ArityZero.rec' yet, consider using 'match ... with' and/or structural recursion"
 def ArityZero.elim (az: ArityZero): T :=
   False.elim (ArityZero.rec az)
+
+instance ArityOne.ofNatZero: OfNat ArityOne 0 := ⟨ArityOne.zth⟩
+
+instance ArityTwo.ofNatZero: OfNat ArityTwo 0 := ⟨ArityTwo.zth⟩
+instance ArityTwo.ofNatOne: OfNat ArityTwo 1 := ⟨ArityTwo.fst⟩
