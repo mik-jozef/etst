7c0e042f2b01cafc5092037ee477fe23e029a502
mik-jozef <mik.jozef@gmail.com>
2025-07-19 08:45:42 +0200

    Nomnom drei

--- Trisets/Trisets.lean
+++ Trisets/Trisets.lean
@@ -30,6 +30,11 @@ def Truth3.and: Truth3 → Truth3 → Truth3
 | t3, b => b
 | a, t3 => a
 
+def Truth3.not: Truth3 → Truth3
+| f3 => t3
+| u3 => u3
+| t3 => f3
+
 
 def Truth3.and_eq_t3
   (eq: Truth3.and a b = t3)
@@ -77,17 +82,17 @@ inductive TriRelation.Expr (T: Type*) where
   A system of trisets (or a universe) is a collection of elements,
   called trisets, together with a three-valued membership relation,
   that satisfies the following properties:
-  
+
   TODO
-  
+
   TODO sth like: In this volume, our goal is to construct an instance
   of `Trisets`.
 -/
 structure Trisets where
   Triset: Type*
-  
+
   Mem: Triset → Triset → Truth3
-  
+
   isExtensional:
     (∀ a b e: Triset, Mem e a = Mem e b) →
     a = b
@@ -96,7 +101,7 @@ structure Trisets where
   exUnion:
     ∀ s0 s1 elem, ∃ s,
       Mem elem s = (Mem elem s0).or (Mem elem s1)
-  
+
   /-
     TODO should be redundant if we have comprehension. Also:
     restricted comprehension? should follow from replacement, right?
@@ -117,21 +122,21 @@ structure Trisets where
 
 namespace Set3Pair
   def PreTriset := Nat
-  
+
   -- Strong (a la "definite") membership, `a ∈ b`.
   def PreTriset.Ins (preElem preTs: PreTriset): Prop :=
     Set3.defMem (Pair.nthSet3 preElem) (Pair.fromNat preTs)
-  
+
   -- Weak (a la "possible") membership, `a ∈? b`.
   def PreTriset.Inw (preElem preTs: PreTriset): Prop :=
     Set3.posMem (Pair.nthSet3 preElem) (Pair.fromNat preTs)
-  
+
   open PreTriset
-  
+
   inductive TransitionLabels where
   | ins
   | inw
-  
+
   def transitionSystem:
     LabeledTransitionSystem PreTriset
   := {
@@ -140,12 +145,12 @@ namespace Set3Pair
       | a, .ins, b => a.Ins b
       | a, .inw, b => a.Inw b
   }
-  
+
   def trisetSetoid: Setoid PreTriset where
     iseqv := isEquivalence transitionSystem
-  
+
   def Triset := Quotient trisetSetoid
-  
+
   structure TrisetIns
     (elem ts: Triset)
     (preElem preTs: PreTriset)
@@ -155,7 +160,7 @@ namespace Set3Pair
     elemEq: elem = ⟦preElem⟧
     tsEq: ts = ⟦preTs⟧
     ins: preElem.Ins preTs
-  
+
   structure TrisetInw
     (elem ts: Triset)
     (preElem preTs: PreTriset)
@@ -165,18 +170,18 @@ namespace Set3Pair
     elemEq: elem = ⟦preElem⟧
     tsEq: ts = ⟦preTs⟧
     inw: preElem.Inw preTs
-  
+
   def Triset.Ins (elem ts: Triset): Prop :=
     ∃ preElem preTs, TrisetIns elem ts preElem preTs
-  
+
   def Triset.Inw (elem ts: Triset): Prop :=
     ∃ preElem preTs, TrisetInw elem ts preElem preTs
-  
+
   noncomputable def Triset.Mem3 (elem ts: Triset): Truth3 :=
     if Triset.Ins elem ts
     then .t3
     else if Triset.Inw elem ts
     then .u3
     else .f3
-  
+
 end Set3Pair
